bb0e92e3a7ccff305cb8a7a4b88b518df3083e3a
Jade <macbook2023m2@Jades-PC.local>
2024-11-22 09:49:07 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Next.js Projectsss</h1>
+        <p>This is the homepage. Start building your app from here!</p>
+      </div>
+    );
+  }
+  
